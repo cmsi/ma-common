4d714afafe4b1ec2ce410412c6980a15e1bd8583
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-03-05 12:45:36 +0900

    update

--- script/build.sh
+++ script/build.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 . $(dirname $0)/path.sh
-test -z $BUILD_DIR && exit 127
+test -z ${BUILD_DIR} && exit 127
 
-mkdir -p $TARGET_DIR
-cd $BUILD_DIR
+CP="scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+
+mkdir -p ${TARGET_DIR}
+cd ${BUILD_DIR}
 ARCH=$(dpkg --print-architecture)
 case ${ARCH} in
-    amd64)
-        dpkg-buildpackage -us -uc
-        mv -f ../${PACKAGE}_${VERSION}_${ARCH}.changes ../${PACKAGE}_${VERSION}_${ARCH}.changes.orig
-        awk '$3!="debug" {print}' ../${PACKAGE}_${VERSION}_${ARCH}.changes.orig > ../${PACKAGE}_${VERSION}_${ARCH}.changes
-        scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ../${PACKAGE}_${VERSION}*.changes ../${PACKAGE}_${VERSION}*.buildinfo ../*_${VERSION}*.deb $TARGET_DIR
-        scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ../${PACKAGE}_${VERSION}*.dsc ../${PACKAGE}_${VERSION}*.debian.tar.* $TARGET_DIR
-        scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ../${PACKAGE}_${VERSION_BASE}.orig.tar.gz $TARGET_DIR
-        ;;
-    *)
-        dpkg-buildpackage -B -us -uc
-        mv -f ../${PACKAGE}_${VERSION}_${ARCH}.changes ../${PACKAGE}_${VERSION}_${ARCH}.changes.orig
-        awk '$3!="debug" {print}' ../${PACKAGE}_${VERSION}_${ARCH}.changes.orig > ../${PACKAGE}_${VERSION}_${ARCH}.changes
-        scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ../${PACKAGE}_${VERSION}*.changes ../*.buildinfo ../*_${VERSION}*.deb $TARGET_DIR
-        ;;
+  amd64)
+    dpkg-buildpackage -us -uc
+    ;;
+  *)
+    dpkg-buildpackage -B -us -uc
+    ;;
 esac
+
+if [ -f ../${PACKAGE}_${VERSION}_${ARCH}.changes ]; then
+  mv -f ../${PACKAGE}_${VERSION}_${ARCH}.changes ../${PACKAGE}_${VERSION}_${ARCH}.changes.orig
+  awk '$3!="debug" {print}' ../${PACKAGE}_${VERSION}_${ARCH}.changes.orig > ../${PACKAGE}_${VERSION}_${ARCH}.changes
+  FILES=$(awk 'section == "files" { print "../"$5 } $1=="Files:" { section = "files" }' ../${PACKAGE}_${VERSION}_${ARCH}.changes)
+  echo "Copying: ../${PACKAGE}_${VERSION}_${ARCH}.changes ${FILES} to ${TARGET_DIR}"
+  ${CP} ../${PACKAGE}_${VERSION}_${ARCH}.changes ${FILES} ${TARGET_DIR}
+fi
